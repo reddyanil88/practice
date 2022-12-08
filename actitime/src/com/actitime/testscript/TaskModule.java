39b0ec2829f5c50aeb9ea51c80fa0fc37655e2eb
Lenovo <Lenovo@Lenovo>
2022-12-08 12:24:50 +0530

    commiting task module to branchX

---
+++ actitime/src/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+
+	@Test
+	public void testcreateTask()
+	{
+		Reporter.log("subha",true);
+	}
+
+}
